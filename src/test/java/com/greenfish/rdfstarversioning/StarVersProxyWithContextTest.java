428683064f525a6c9c75330ed625504b61db8926
Filip Kovacevic <f.kovacevic@gmx.at>
2022-04-15 15:37:20 +0200

    <test>: Separate tests with context into a new test class.

--- src/test/java/com/greenfish/rdfstarversioning/StarVersProxyWithContextTest.java
+++ src/test/java/com/greenfish/rdfstarversioning/StarVersProxyWithContextTest.java
@@ -10,7 +10,6 @@ import org.eclipse.rdf4j.repository.config.RepositoryConfigException;
 import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 import org.eclipse.rdf4j.rio.RDFHandlerException;
 import org.eclipse.rdf4j.rio.RDFParseException;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -25,7 +24,7 @@ import static org.junit.Assert.*;
 /**
  * Tests the example plugin.
  */
-public class StarVersProxyTest {
+public class StarVersProxyWithContextTest {
     private static SPARQLRepository repo;
     private static SPARQLRepository proxy;
     private static RepositoryConnection sparqlRepoConnection;
@@ -127,10 +126,10 @@ public class StarVersProxyTest {
     }
 
     @Test
-    public void insertSingleTripleTest() throws InterruptedException {
-        defaultGraph = true;
-        String triple = "<http://example.com/s/insertThis1> <http://example.com/p/insertThis1> <http://example.com/o/insertThis1>";
-        String updateString = String.format("insert data {%s}", triple);
+    public void insertSingleTripleWithContextTest() throws InterruptedException {
+        defaultGraph = false;
+        String triple = "<http://example.com/testGraph/s/insertThis1> <http://example.com/testGraph/p/insertThis1> <http://example.com/testGraph/o/insertThis1>";
+        String updateString = String.format("insert data { graph <http://example.com/testGraph> {%s}}", triple);
         sparqlProxyConnection.begin();
         sparqlProxyConnection.prepareUpdate(updateString).execute();
         sparqlProxyConnection.commit();
@@ -138,51 +137,60 @@ public class StarVersProxyTest {
         //Wait for plugin to insert triples. This is managed by the server.
         Thread.sleep(5000);
 
-        TupleQuery query = sparqlRepoConnection.prepareTupleQuery(String.format("select * { <<<<%s>> ?x ?y>> ?a ?b }",triple));
+        TupleQuery query = sparqlRepoConnection.prepareTupleQuery(String.format("select * from <http://example.com/testGraph> { <<<<%s>> ?x ?y >> ?a ?b }",triple));
         try (TupleQueryResult result = query.evaluate()) {
-            assertTrue("Must have one double-nested triples in the result", result.hasNext());
+            assertTrue("Must have one double-nested triple in the result", result.hasNext());
             assertEquals(1, result.stream().count());
         }
     }
 
     @Test
-    public void insertMultipleTriplesTest() throws InterruptedException {
-        defaultGraph = true;
-        String triple1 = "<http://example.com/s/insertThis2> <http://example.com/p/insertThis2> <http://example.com/o/insertThis2>";
-        String triple2 = "<http://example.com/s/insertThis3> <http://example.com/p/insertThis3> <http://example.com/o/insertThis3>";
-        String updateString = String.format("insert data {%s. %s .}", triple1, triple2);
-        sparqlProxyConnection.begin();
-        sparqlProxyConnection.prepareUpdate(updateString).execute();
-        sparqlProxyConnection.commit();
+    public void insertMultipleTriplesWithContextTest() throws InterruptedException {
+        defaultGraph = false;
+        String triple1 = "<http://example.com/testGraph/s/insertThis2> <http://example.com/testGraph/p/insertThis2> <http://example.com/testGraph/o/insertThis2>";
+        String triple2 = "<http://example.com/testGraph/s/insertThis3> <http://example.com/testGraph/p/insertThis3> <http://example.com/testGraph/o/insertThis3>";
+
+        String updateString = String.format("insert data { graph <http://example.com/testGraph> {%s . %s .}}",
+                triple1, triple2);
+        sparqlRepoConnection.begin();
+        sparqlRepoConnection.prepareUpdate(updateString).execute();
+        sparqlRepoConnection.commit();
 
         //Wait for plugin to insert triples. This is managed by the server.
         Thread.sleep(5000);
 
         TupleQuery query = sparqlRepoConnection.prepareTupleQuery(
-                String.format("select * { {<<<<%s>> ?x ?y>> ?a ?b} union {<<<<%s>> ?x ?y>> ?a ?b} }", triple1, triple2));
+                String.format("select * from <http://example.com/testGraph> { {<<<<%s>> ?x ?y>> ?a ?b} union {<<<<%s>> ?x ?y>> ?a ?b} }",
+                        triple1, triple2));
         try (TupleQueryResult result = query.evaluate()) {
-            assertTrue("Must have two double-nested triples in the result.", result.hasNext());
+            assertTrue("Must two double-nested triples in the result.", result.hasNext());
             assertEquals(2, result.stream().count());
+
         }
     }
 
     @Test
-    public void deleteSingleTripleTest() throws InterruptedException {
-        defaultGraph = true;
-        String updateString;
-        String triple = "<http://example.com/s/deleteThis1> <http://example.com/p/deleteThis1> <http://example.com/o/deleteThis1>";
+    public void deleteSingleTripleWithContextTest() throws InterruptedException {
+        defaultGraph = false;
 
-        updateString = String.format("delete data {%s}", triple);
-        sparqlProxyConnection.begin();
-        sparqlProxyConnection.prepareUpdate(updateString).execute();
-        sparqlProxyConnection.commit();
+        //Delete
+        String triple = "<http://example.com/testGraph/s/deleteThis2> <http://example.com/testGraph/p/deleteThis2> <http://example.com/testGraph/o/deleteThis2>";
+        String updateString = String.format("delete data { graph <http://example.com/testGraph> { %s }}", triple);
+        sparqlRepoConnection.begin();
+        sparqlRepoConnection.prepareUpdate(updateString).execute();
+        sparqlRepoConnection.commit();
 
-        Thread.sleep(1000);
+        Thread.sleep(5000);
 
         TupleQuery query = sparqlRepoConnection.prepareTupleQuery(String.format("select * { <<<<%s>> ?x ?y>> ?a ?b }", triple));
+        try (TupleQueryResult result = query.evaluate()) {
+            assertTrue("Number of triples should not change in the default graph", result.hasNext());
+        }
+
+        query = sparqlRepoConnection.prepareTupleQuery(String.format("select * from <http://example.com/testGraph> { <<<<%s>> ?x ?y>> ?a ?b }", triple));
 
         try (TupleQueryResult result = query.evaluate()) {
-            assertTrue("Number of triples should not change in the default graph.", result.hasNext());
+            assertTrue("Number of triples should not change in graph <http://example.com/testGraph>.", result.hasNext());
             int c = 0;
             while (result.hasNext()) {
                 BindingSet bs = result.next();
@@ -199,143 +207,39 @@ public class StarVersProxyTest {
     }
 
     @Test
-    public void deleteMultipleTripleTest() throws InterruptedException {
-        defaultGraph = true;
-        String updateString;
-        String triple1 = "<http://example.com/s/deleteThis2> <http://example.com/p/deleteThis2> <http://example.com/o/deleteThis2>";
-        String triple2 = "<http://example.com/s/deleteThis3> <http://example.com/p/deleteThis3> <http://example.com/o/deleteThis3>";
-
-
-        updateString = String.format("delete data {%s . %s .}", triple1, triple2);
-        sparqlProxyConnection.begin();
-        sparqlProxyConnection.prepareUpdate(updateString).execute();
-        sparqlProxyConnection.commit();
-
-        Thread.sleep(5000);
-
-        TupleQuery query = sparqlRepoConnection.prepareTupleQuery(
-                String.format("select * { {<<<<%s>> ?x ?y>> ?a ?b} union {<<<<%s>> ?x ?y>> ?a ?b} }", triple1, triple2));
-
-        try (TupleQueryResult result = query.evaluate()) {
-            assertTrue("Number of triples should not change in the default graph.", result.hasNext());
-            int c = 0;
-            while (result.hasNext()) {
-                BindingSet bs = result.next();
-                //String s = bs.getValue("s").stringValue();
-                String p = bs.getValue("a").stringValue();
-                String o = bs.getValue("b").stringValue();
-                c++;
-                System.out.println(p + " " + o);
-                assertNotEquals("9999-12-31T00:00:00.000+00:00", o);
-
-            }
-            assertEquals(2, c);
-        }
+    public void deleteMultipleTripleWithContextTest() {
+        defaultGraph = false;
+        fail("not yet implemented");
     }
 
     @Test
-    public void deleteNonExistingTest() {
+    public void deleteNonExistingWithContextTest() {
         defaultGraph = true;
         fail("not yet implemented");
     }
 
     @Test
-    public void deleteAllTriplesTest() {
-        defaultGraph = true;
+    public void deleteAllTriplesWithContextTest() {
+        defaultGraph = false;
         fail("not yet implemented");
     }
 
     @Test
-    public void queryLiveDataOneStatementTest() {
-        defaultGraph = true;
-        TupleQuery query = sparqlProxyConnection.prepareTupleQuery("select * { ?s <http://example.com/queries/predicate1> ?o .}");
-
-        try (TupleQueryResult result = query.evaluate()) {
-            int c = 0;
-            while (result.hasNext()) {
-                BindingSet bs = result.next();
-                String s = bs.getValue("s").stringValue();
-                String o = bs.getValue("o").stringValue();
-                c++;
-                assertEquals("http://example.com/queries/queryThisSubject2", s);
-                assertEquals("http://example.com/queries/queryThisObject2", o);
-            }
-            assertEquals(1, c);
-        }
-    }
-
-    @Test
-    public void queryLiveDataSequencePathTest() {
+    public void queryLiveWithContextDataTest() {
+        defaultGraph = false;
         fail("not yet implemented");
     }
 
     @Test
-    public void queryLiveDataMultiBGPTest() {
+    public void queryHistoryWithContextDataTest() {
+        defaultGraph = false;
         fail("not yet implemented");
     }
 
     @Test
-    public void queryHistoryDataTest() {
-        defaultGraph = true;
+    public void insertDeleteReInsertWithContextTest() {
+        defaultGraph = false;
         fail("not yet implemented");
-    }
-
-    @Test
-    public void insertDeleteReInsertDeleteTest() throws InterruptedException {
-        //TODO: Problem with concurrent updates. Consecutively inserting, deleting and re-inserting
-        // leads to ignorance of the delete handling by the plugin.
-        /*
-         DB requests
-         user: insert
-         plugin: pluginUpdateRequestCommitted=true
-         plugin-insert template: delete block
-         user: delete (does not get processed by plugin because pluginUpdateRequestCommitted=true)
-         plugin-insert template: insert block
-         plugin: pluginUpdateRequestCommitted=false
-
-         */
-        defaultGraph = true;
-        String triple = "<http://example.com/test#reinsertThisS1> <http://example.com/test#p> <http://example.com/test#reinsertThisO1>";
-        String updateString = String.format("insert data {%s}", triple);
-        sparqlProxyConnection.begin();
-        sparqlProxyConnection.prepareUpdate(updateString).execute();
-        sparqlProxyConnection.commit();
-        Thread.sleep(500);
-
-        updateString = String.format("delete data {%s}", triple);
-        sparqlProxyConnection.begin();
-        sparqlProxyConnection.prepareUpdate(updateString).execute();
-        sparqlProxyConnection.commit();
-
-        updateString = String.format("insert data {%s}", triple);
-        sparqlProxyConnection.begin();
-        sparqlProxyConnection.prepareUpdate(updateString).execute();
-        sparqlProxyConnection.commit();
-        Thread.sleep(500);
-
-        updateString = String.format("delete data {%s}", triple);
-        sparqlProxyConnection.begin();
-        sparqlProxyConnection.prepareUpdate(updateString).execute();
-        sparqlProxyConnection.commit();
-        //Wait for plugin to insert triples. This is managed by the server.*/
-        Thread.sleep(2000);
-
-        TupleQuery query = sparqlRepoConnection.prepareTupleQuery(String.format("select * { <<<<%s>> ?x ?y>> ?a ?b }",triple));
-        try (TupleQueryResult result = query.evaluate()) {
-            assertEquals(1, result.stream().count());
-        }
-        try (TupleQueryResult result = query.evaluate()) {
-            while (result.hasNext()) {
-                BindingSet bs = result.next();
-                //String s = bs.getValue("s").stringValue();
-                String p = bs.getValue("a").stringValue();
-                String o = bs.getValue("b").stringValue();
-                System.out.println(p + " " + o);
-                assertNotEquals("9999-12-31T00:00:00.000+00:00", o);
-
-            }
-        }
-
 
     }
 
